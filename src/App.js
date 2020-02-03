e323838bbc4a948598d9fb634da45422965047be
Masumba <masumbakayombo1@gmail.com>
2020-02-03 17:07:10 +0200

    Added bootstrap
    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home} from './components/Home';
+import {Contact} from './components/Contact';
+import {About} from './components/About';
+import {NoMatch} from './components/NoMatch';
+import {Layout} from './components/ui/layout';
+import {NavigationBar} from './components/ui/navigationBar';
+import {Jumbotron} from './components/ui/Jumbotron';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <NavigationBar/>
+        <Jumbotron/>
+        <Layout>
+        <Router>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/contact" component={Contact}/>
+              <Route path="/about" component={About}/>
+              <Route component={NoMatch}/>
+            </Switch>
+          </Router>
+          
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
